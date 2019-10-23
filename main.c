f30f484886f24299b1ca8281a358476c32286c4d
LucaTNV <lucafanari85@hotmail.com>
2019-10-23 11:35:28 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char cambiaLettera(char lettera);
+
+int main() {
+    char carattereUtente; //Carattere inserito dall'utente
+
+    printf("Inserire un carattere: \n");
+    scanf("%c", &carattereUtente); //Acquisizione carattere
+
+    //Stampa risultato funzione
+    printf("%c -> %c\n", carattereUtente, cambiaLettera(carattereUtente));
+
+    return 0;
+}
+
+/**
+ * Restituisce il corrispondente carattere maiuscolo se minuscolo e viceversa.
+ * @param lettera Lettera di input
+ * @return Carattere cambiato oppure il carattere '?' se non è una il carattere in input non è una lettera
+ */
+char cambiaLettera(char lettera) {
+    if (lettera >= 'a' && lettera <= 'z')       //Lettere minuscole
+        return (char) ('A' + lettera - 'a');
+    else if (lettera >= 'A' && lettera <= 'Z')  //Lettere maiuscole
+        return (char) ('a' + lettera - 'A');
+    else                                        //Carattere non lettera
+        return '?';
+}
